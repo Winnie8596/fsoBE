f5e9a1149e9b7e05df373a5fc2d273608c6e0260
Winnie Ngu <w4winnie97@gmail.com>
2024-08-04 15:39:12 +0800

    Stopped tracking .env File

--- mongo.js
+++ mongo.js
@@ -10,17 +10,12 @@ const password = process.argv[2];
 const name = process.argv[3];
 const number = process.argv[4];
 
-// Print out the password and URL for debugging
-// console.log("Password:", password);
-
-// const url = `mongodb+srv://w4winnie97:${password}@cluster0.voebmho.mongodb.net/personApp`;
-
-// console.log("MongoDB URL:", url); // Print the URL to check its format
-
 mongoose
-  .connect(process.env.MONGO_URI, {
+  .connect(process.env.MONGODB_URI, {
+    useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useFindAndModify: false,
   })
   .then(() => {
     console.log("Connected to MongoDB");
